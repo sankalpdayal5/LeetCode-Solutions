6f849476e8fdc129709b1a73a3ddee4ad4e3e0bd
Jevin Vekaria <jevin925@gmail.com>
2020-10-21 22:43:30 +0530

    Create 0001.py

---
+++ Python/0001.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+            dict = {}
+            for i, v in enumerate(nums):
+                if target - v in dict:
+                    return dict[target - v] , i 
+                elif v not in dict:
+                    dict[v] = i
+            return []
