b4c8f24d47ef5a3bf7d882274788a9c97858a439
amulya12 <30744874+amulya12@users.noreply.github.com>
2020-10-21 16:10:32 +0530

    Create 0055.java

---
+++ Java/0055.java
@@ -0,0 +1,25 @@
+enum Index {
+    GOOD, BAD, UNKNOWN
+}
+
+public class Solution {
+    public boolean canJump(int[] nums) {
+        Index[] memo = new Index[nums.length];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[memo.length - 1] = Index.GOOD;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int furthestJump = Math.min(i + nums[i], nums.length - 1);
+            for (int j = i + 1; j <= furthestJump; j++) {
+                if (memo[j] == Index.GOOD) {
+                    memo[i] = Index.GOOD;
+                    break;
+                }
+            }
+        }
+
+        return memo[0] == Index.GOOD;
+    }
+}
