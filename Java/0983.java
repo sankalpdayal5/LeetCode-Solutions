a7dbcb55fe77ccc3adb612f2a87d043729249b0c
Srishti Singh <55200741+SrishtiSingh-hub@users.noreply.github.com>
2020-10-21 23:11:09 +0530

    Update 0983.java

--- Java/0983.java
+++ Java/0983.java
@@ -24,16 +24,3 @@ class Solution {
 }
 
 
-/*
-Time - O(n)
-Space - O(n)
-
-
-dp[i] storing cost of travel upto i days
-
-dp[i] = dp[i-1] + costs[0]
-dp[i] = Math.min(dp[i], dp[Math.max(0, i-7)] + costs[1])
-dp[i] = Math.min(dp[i], dp[Math.max(0, i-30)] + costs[2])
-
-
-*/
