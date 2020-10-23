f96110084dd08a6ecd863367992d0a72bbe11ac3
Sanya Pandey <55851287+SanyaPandey-hub@users.noreply.github.com>
2020-10-24 01:26:40 +0530

    Update 0121.py

--- Python/0121.py
+++ Python/0121.py
@@ -1,12 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        z = 9999999999999999999
+        min_val = 9999999999999999999
         res = 0
-        for i, y in enumerate(prices):
-            if y < z:
-                z = y
-            elif y > z:
-                res = max(res, y-z)
+        for i, current_val in enumerate(prices):
+            if current_val < min_val:
+                min_val = current_val
+            elif current_val > min_val:
+                res = max(res, current_val-min_val)
         return res
         
 
