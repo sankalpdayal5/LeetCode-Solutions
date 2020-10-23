9163cb4890cc01a153c85d006711aab63c0b2c3f
Darshita Kumar <kumardarshita18@gmail.com>
2020-10-23 13:49:29 +0530

    Removed commented question from the py file

--- Python/1347.py
+++ Python/1347.py
@@ -1,23 +1,3 @@
-'''
-Leetcode 1347
-Given two equal-size strings s and t. In one step you can choose any character of t and replace it with another character.
-
-Return the minimum number of steps to make t an anagram of s.
-
-An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
-
-
-Example 1:
-Input: s = "bab", t = "aba"
-Output: 1
-Explanation: Replace the first 'a' in t with b, t = "bba" which is anagram of s.
-
-Example 2:
-Input: s = "leetcode", t = "practice"
-Output: 5
-Explanation: Replace 'p', 'r', 'a', 'i' and 'c' from t with proper characters to make t anagram of s.
-
-'''
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
         map_s = {}
