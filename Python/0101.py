d41a4d695330b4d538a4ca1c45f30f12d578bc5e
Sankalp Dayal <sankalpdayal5@gmail.com>
2020-10-23 12:05:07 +0530

    Update 0101.py

--- Python/0101.py
+++ Python/0101.py
@@ -5,17 +5,16 @@
 #         self.left = left
 #         self.right = right
 class Solution: 
-     def isSymmetric(self, root: TreeNode) -> bool:
-         return isMirror(root, root)
+    def isSymmetric(self, root: TreeNode) -> bool:
+        return isMirror(root, root)
         
-def isMirror(root1, root2):
+    def isMirror(root1, root2):
+        if root1 is None and root2 is None:
+            return True
 
-             if root1 is None and root2 is None:
-                return True
+        if (root1 is not None and root2 is not None):
+            if root1.val == root2.val:
+                return (isMirror(root1.left, root2.right) and isMirror(root1.right, root2.left))
 
-             if (root1 is not None and root2 is not None):
-                if root1.val == root2.val:
-                    return (isMirror(root1.left, root2.right) and isMirror(root1.right, root2.left))
-
-             return False
+        return False
 
